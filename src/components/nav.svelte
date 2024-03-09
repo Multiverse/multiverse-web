ba94b3a42312bca3188f514cedae41852de484e6
Zax71 <zabax32@gmail.com>
2024-03-09 13:14:43 +0000

    Add navbar

---
+++ src/components/nav.svelte
@@ -0,0 +1,27 @@
+<script>
+	import Logo from '$lib/images/logo/square.png';
+	import Discord from '~icons/logos/discord-icon';
+	import GitHub from '~icons/logos/github-icon';
+</script>
+
+<nav class="px-1 border-b-[1px] border-b-black">
+	<div class="flex items-center">
+		<a href="/"><img src={Logo} alt="Multiverse logo" class="w-10 hover:scale-110" /></a>
+		<a href="/wiki" class="button">Wiki</a>
+		<a href="/downloads" class="button">Downloads</a>
+
+		<div class="ml-auto flex items-center">
+			<a href="https://discord.gg/NZtfKky" class="text-xl hover:scale-110"><Discord /></a>
+			<a href="https://github.com/Multiverse" class="text-xl hover:scale-110"><GitHub /></a>
+		</div>
+	</div>
+</nav>
+
+<style>
+	a {
+		@apply m-1;
+	}
+	.button {
+		@apply bg-red-500 rounded-full px-2 hover:bg-red-600;
+	}
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import Icons from 'unplugin-icons/vite';
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		Icons({
+			compiler: 'svelte'
+		})
+	],
 	server: {
 		port: 3000,
 		open: true,
