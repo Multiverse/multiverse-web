833a8e25ddc51597f8d11a8fbcb9acb009735b84
Zax71 <zabax32@gmail.com>
2024-03-09 11:04:11 +0000

    Lets start again...

--- src/routes/+layout.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import '../app.css';
-	import Nav from '../components/nav.svelte';
-</script>
-
-<Nav />
-<slot />
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	import IconAccessibility from '~icons/carbon/accessibility';
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<IconAccessibility />
