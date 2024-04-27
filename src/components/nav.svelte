ff6b209fb8d7503c233dff2663c2c01c8f586181
Zax71 <zabax32@gmail.com>
2024-04-27 11:05:19 +0100

    Add home to nav and make downloads the cta

--- src/components/nav.svelte
+++ src/components/nav.svelte
@@ -7,10 +7,9 @@
 <nav class="px-2 border-b-[1px] border-b-black">
 	<div class="flex items-center">
 		<a href="/"><img src={Logo} alt="Multiverse logo" class="w-10 hover:scale-110" /></a>
-		<a href="/wiki" class="text-lg rounded-lg px-2 hover:opacity-85 bg-primary text-background"
-			>Wiki</a
-		>
-		<a href="/downloads" class="text-lg rounded-lg px-2 hover:opacity-85 bg-secondary">Downloads</a>
+		<a href="/" class="button bg-secondary">Home</a>
+		<a href="/wiki" class="button bg-secondary">Wiki</a>
+		<a href="/downloads" class="button cta">Downloads</a>
 
 		<div class="ml-auto flex items-center">
 			<a href="https://discord.gg/NZtfKky" class="text-2xl hover:scale-110"><Discord /></a>
@@ -22,5 +21,13 @@
 <style>
 	a {
 		@apply m-1;
+	}
+
+	.cta {
+		@apply bg-primary text-background;
+	}
+
+	.button {
+		@apply text-lg rounded-lg px-2 hover:opacity-85;
 	}
 </style>
