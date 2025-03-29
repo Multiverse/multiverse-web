d9885a3b3178b0c3ed2fcb5b8f13ce45ef4566dd
Ben Woo <30431861+benwoo1110@users.noreply.github.com>
2025-03-29 15:04:04 +0800

    Add MV5 information to download page

--- src/routes/download/plugins/+page.svelte
+++ src/routes/download/plugins/+page.svelte
@@ -37,6 +37,16 @@
       tagColor: "bg-green-500",
     },
   ];
+
+  let features = [
+    "Revamped command structure with full tab-completion",
+    "Revamped config file with detailed comments and more options",
+    "Revamped teleport handling with finer per-world permissions",
+    "Significant improvements to last_location! ",
+    "Added world creation options such as Single Biome and customizable flat world with Generator Settings",
+    "Fixed Multiverse-Inventories player name change bug!",
+    "Much much much more, full changelog in discord!",
+  ];
 </script>
 
 <main>
@@ -53,7 +63,7 @@
               {plugin.tag}
             </span>
           </div>
-          <div class="text-gray-600">{plugin.description}</div>
+          <div class="text-gray-500">{plugin.description}</div>
         </div>
         <a
           class="svp-gradient-bg text-white dark:text-warm-gray-8 px-4 py-2 rounded-full hover:bg-blue-600 flex items-center gap-2 mt-4 sm:mt-0 flex-none"
@@ -67,5 +77,39 @@
         </a>
       </div>
     {/each}
+  </div>
+  <div class="mt-12">
+    <h2>Exciting NEW Multiverse 5 Version</h2>
+    <p class="text-gray-500">
+      We are releasing very early builds of Multiverse version 5. To get these very early builds, head to our
+      <a href="https://discord.gg/NZtfKky" class="underline text-gray-400">Discord Server</a>.
+    </p>
+    <h3>Whats New?</h3>
+    <ul class="list-disc pl-5 space-y-2">
+      {#each features as feature}
+        <li class="text-gray-500">
+          {feature}
+        </li>
+      {/each}
+    </ul>
+    <h3>Why Still Alpha?</h3>
+    <p class="text-gray-500">
+      While the gameplay features are now relatively stable, there is still a lot of backend work, especially in the developer API and documentation side of things.
+      MV5 <b>does not have a stable API</b> as we are still hard at work experimenting and fine tuning internally to ensure a good foundation is built for future updates 
+      and maintainability.
+    </p>
+    <h3>Call for help with MV5 wiki</h3>
+    <p class="text-gray-500">
+      The MV5 updates are great, but we need to inform people on how to use them! However writing documentation is a long and laborious process which will take away more dev time. 
+      Thus, we are looking for volunteers to help in writing usage guides. Our wiki is now hosted on a web page, and the source code is here: https://github.com/Multiverse/multiverse-web. 
+      The wiki itself is using markdown formatting. If you are not sure on how to use GitHub, you can send your work via discord too!
+    </p>
+    <h3 class="text-red-500">WARNINGGGG!!</h3>
+    <ul class="list-disc pl-5 space-y-2 text-red-500">
+      <li>Only supports <b class="underline">MINECRAFT 1.18 AND ABOVE</b></li>
+      <li>Alpha Builds are expected to have <b class="underline">BUGS</b></li>
+      <li><b class="underline">MAKE BACKUPS</b> before updating to Alpha Builds</li>
+      <li><b class="underline">IT WILL BE IMPOSSIBLE TO REVERT</b> to older multiverse version (unless you have backups)</li>
+    </ul>
   </div>
 </main>
